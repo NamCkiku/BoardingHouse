365ff5ac332a91653db985805f6a4c4e598e4f63
NamCkiku <tranhoangnam11373@gmail.com>
2017-02-21 21:28:01 +0700

    add content Administrator

---
+++ BoaringHouse.API/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BoaringHouse.API.Startup))]
+namespace BoaringHouse.API
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
