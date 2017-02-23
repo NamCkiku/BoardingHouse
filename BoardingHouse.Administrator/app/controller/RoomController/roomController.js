5790257369d345cf83f889b5aed3a283701363af
NamCkiku <tranhongnam11373@gmail.com>
2017-02-23 13:55:40 +0700

    add modal

--- BoardingHouse.Administrator/app/controller/RoomController/roomController.js
+++ BoardingHouse.Administrator/app/controller/RoomController/roomController.js
@@ -1,9 +1,9 @@
 ﻿(function (app) {
     app.controller('roomController', roomController);
 
-    roomController.$inject = ['$scope'];
+    roomController.$inject = ['$scope', 'blockUI', '$modal', '$rootScope'];
 
-    function roomController($scope) {
+    function roomController($scope, blockUI, $modal, $rootScope) {
         $scope.filter = {
             Keywords: "",
             StartDate: "",
@@ -12,6 +12,114 @@
             searchByEndDate: true,
             Status: true
         }
-    }
+        function load() {
+            //var myBlockUI = blockUI.instances.get('BlockUIRoom');
+            //myBlockUI.start();
+            //myBlockUI.stop();
+        }
+        load();
+        var __appBasePath = $rootScope.baseUrl;
+        $scope.editorOptions = {
+            tools: [
+                    "bold",
+                    "italic",
+                    "underline",
+                    "strikethrough",
+                    "justifyLeft",
+                    "justifyCenter",
+                    "justifyRight",
+                    "justifyFull",
+                    "insertUnorderedList",
+                    "insertOrderedList",
+                    "indent",
+                    "outdent",
+                    "createLink",
+                    "unlink",
+                    "insertImage",
+                    "insertFile",
+                    "subscript",
+                    "superscript",
+                    "createTable",
+                    "addRowAbove",
+                    "addRowBelow",
+                    "addColumnLeft",
+                    "addColumnRight",
+                    "deleteRow",
+                    "deleteColumn",
+                    "viewHtml",
+                    "formatting",
+                    "cleanFormatting",
+                    "fontName",
+                    "fontSize",
+                    "foreColor",
+                    "backColor",
+                    "print"
+            ],
 
+            imageBrowser: {
+                messages: {
+                    dropFilesHere: "Drop files here"
+                },
+                transport: {
+                    type: "imagebrowser-aspnetmvc",
+                    read: __appBasePath + "ImageBrowser/Read",
+                    destroy: {
+                        url: __appBasePath + "ImageBrowser/Destroy",
+                        type: "POST"
+                    },
+                    create: {
+                        url: __appBasePath + "ImageBrowser/Create",
+                        type: "POST"
+                    },
+                    uploadUrl: __appBasePath + "ImageBrowser/upload",
+                    imageUrl:"/viewerImages/UserFiles/Images/{0}",
+                    thumbnailUrl: __appBasePath + "ImageBrowser/thumbnail"
+                }
+            },
+            fileBrowser: {
+                messages: {
+                    dropFilesHere: "Drop files here"
+                },
+
+                transport: {
+                    type: "imagebrowser-aspnetmvc",
+                    read: __appBasePath + "FileBrowser/Read",
+                    destroy: {
+                        url: __appBasePath + "FileBrowser/Destroy",
+                        type: "POST"
+                    },
+                    create: {
+                        url: __appBasePath + "FileBrowser/Create",
+                        type: "POST"
+                    },
+                    uploadUrl: __appBasePath + "FileBrowser/upload",
+                    fileUrl: __appBasePath + "FileBrowser/File?path={0}"
+                }
+            }
+        };
+        $scope.openModal = openModal;
+        function openModal() {
+            $scope.modalInstance = $modal.open({
+                animation: true,
+                templateUrl: 'Modal.html',
+                backdrop: 'static',
+                windowClass: 'center-modal',
+                scope: $scope,
+                keyboard: false,
+                size: 'lg'
+            });
+            $scope.ok = function () {
+                $scope.modalInstance.dismiss('cancel');
+            };
+            $scope.close = function () {
+                $scope.modalInstance.dismiss('cancel');
+            };
+            $scope.modalInstance.rendered.then(function (response) {
+            })
+            $scope.modalInstance.result.then(function (response) {
+
+            }, function () {
+            });
+        }
+    }
 })(angular.module('myApp'));
