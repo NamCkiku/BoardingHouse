f70e8d32f3019bb6d97f9e05ffa5a0945c35a184
NamCkiku <tranhongnam11373@gmail.com>
2017-02-24 11:27:53 +0700

    add report

---
+++ BoardingHouse.Administrator/Controllers/ReportController.cs
@@ -0,0 +1,130 @@
+﻿using Stimulsoft.Report;
+using Stimulsoft.Report.Export;
+using Stimulsoft.Report.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoardingHouse.Administrator.Controllers
+{
+    public class ReportController : Controller
+    {
+        public string ReportTempFolder = "~/ReportTemplates/";
+        public ActionResult _Report()
+        {
+            return View();
+        }
+        public ActionResult GetReportSnapshot()
+        {
+            var TemplateName = TempData.ContainsKey("sReportFileName") ? TempData["sReportFileName"].ToString() : string.Empty;
+            try
+            {
+                var report = new StiReport();
+                TempData["dataExportFileTres"] = report;
+                report.Load(Server.MapPath("~/ReportTemplates/Report.mrt"));
+                report.RegData("Orders", ""); ;
+                return StiMvcViewer.GetReportSnapshotResult(report);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public ActionResult ViewerEvent()
+        {
+            return StiMvcViewer.ViewerEventResult();
+        }
+        public ActionResult ExportFilePrint()
+        {
+            try
+            {
+                StiReport report = (StiReport)TempData["dataExportFileTres"];
+                var stream = new MemoryStream();
+
+                var settings = new StiPdfExportSettings();
+                settings.AutoPrintMode = StiPdfAutoPrintMode.Dialog;
+
+                var service = new StiPdfExportService();
+
+                service.ExportPdf(report, stream, settings);
+
+                this.Response.Buffer = true;
+                this.Response.ClearContent();
+                this.Response.ClearHeaders();
+                this.Response.ContentType = "application/pdf";
+                this.Response.ContentEncoding = Encoding.UTF8;
+
+                this.Response.AddHeader("Content-Length", stream.Length.ToString());
+                this.Response.BinaryWrite(stream.ToArray());
+                this.Response.End();
+
+                TempData["dataExportFileTres"] = report;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return View();
+        }
+
+        public ActionResult ExportFilePdf()
+        {
+            try
+            {
+                StiReport report = (StiReport)TempData["dataExportFileTres"];
+                var stream = new MemoryStream();
+                var settings = new StiPdfExportSettings();
+
+                var service = new StiPdfExportService();
+                service.ExportPdf(report, stream);
+
+                this.Response.ClearContent();
+                this.Response.ClearHeaders();
+                this.Response.ContentType = "application/json";
+                this.Response.ContentEncoding = Encoding.UTF8;
+                this.Response.AddHeader("content-disposition", "attachment; filename=Report.PDF");
+                this.Response.BinaryWrite(stream.ToArray());
+                this.Response.End();
+
+                TempData["dataExportFileTres"] = report;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return View();
+        }
+
+        public ActionResult ExportFileExcel()
+        {
+            try
+            {
+                StiReport report = (StiReport)TempData["dataExportFileTres"];
+
+                var stream = new MemoryStream();
+
+                var service = new StiExcelExportService();
+                service.ExportExcel(report, stream);
+
+                this.Response.Clear();
+                this.Response.ContentType = "text/csv";
+                this.Response.AddHeader("content-disposition", "attachment;filename=Report.xls");
+                this.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+                this.Response.BinaryWrite(stream.ToArray());
+                this.Response.End();
+
+                TempData["dataExportFileTres"] = report;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return View();
+        }
+    }
+}
