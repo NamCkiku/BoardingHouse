172c66221310fa7d39ba84598d595b8dd145e411
NamCkiku <tranhongnam11373@gmail.com>
2017-02-28 18:03:49 +0700

    add directive

--- BoardingHouse.Administrator/Controllers/RoomController.cs
+++ BoardingHouse.Administrator/Controllers/RoomController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BoardingHouse.Service.IService;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,39 @@ using System.Web.Mvc;
 
 namespace BoardingHouse.Administrator.Controllers
 {
-    public class RoomController : Controller
+    public class RoomController : BaseController
     {
+        private readonly IRoomService _roomService;
+        public RoomController(IRoomService roomService)
+        {
+            this._roomService = roomService;
+        }
         // GET: Room
         public ActionResult Room()
         {
             return View();
+        }
+        public JsonResult LoadAllRoom()
+        {
+            JsonResult jsonResult = new JsonResult();
+            try
+            {
+                HttpRequestBase request = this.HttpContext.Request;
+                if (ValidateRequestHeader(request))
+                {
+                    jsonResult = Json(new { success = true, lstData = _roomService.GetAll() }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    jsonResult = Json(new { success = false }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception ex)
+            {
+                jsonResult = Json(new { success = false }, JsonRequestBehavior.AllowGet);
+                throw ex;
+            }
+            return jsonResult;
         }
     }
 }
