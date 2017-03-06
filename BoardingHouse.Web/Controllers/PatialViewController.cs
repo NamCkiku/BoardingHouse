343f1c06523a75604d3d2a66e2933c1008c6b4e6
NamCkiku <tranhongnam11373@gmail.com>
2017-03-06 13:39:34 +0700

    add login

---
+++ BoardingHouse.Web/Controllers/PatialViewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoardingHouse.Web.Controllers
+{
+    public class PatialViewController : Controller
+    {
+        // GET: PatialView
+        public PartialViewResult _Header()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ BoardingHouse.Web/app/Controller/AccountController/headerController.js
@@ -0,0 +1,32 @@
+﻿(function (app) {
+    app.controller('headerController', headerController);
+
+    headerController.$inject = ['$scope', 'blockUI', '$modal', '$rootScope'];
+
+    function headerController($scope, blockUI, $modal, $rootScope) {
+        $scope.openLoginModal = openLoginModal;
+        function openLoginModal() {
+            $scope.modalInstance = $modal.open({
+                animation: true,
+                templateUrl: 'ModalLogin.html',
+                backdrop: 'static',
+                windowClass: 'center-modal',
+                scope: $scope,
+                keyboard: false,
+                size: 'lg'
+            });
+            $scope.ok = function () {
+                $scope.modalInstance.dismiss('cancel');
+            };
+            $scope.close = function () {
+                $scope.modalInstance.dismiss('cancel');
+            };
+            $scope.modalInstance.rendered.then(function (response) {
+            })
+            $scope.modalInstance.result.then(function (response) {
+
+            }, function () {
+            });
+        }
+    }
+})(angular.module('myApp'));
