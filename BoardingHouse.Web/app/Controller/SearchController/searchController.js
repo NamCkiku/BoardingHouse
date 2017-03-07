b0f41e5ab71b1a415ce222dae49a873d6f6e80c4
NamCkiku <tranhongnam11373@gmail.com>
2017-03-07 17:29:34 +0700

    add create rooom

---
+++ BoardingHouse.Web/app/Controller/SearchController/searchController.js
@@ -0,0 +1,20 @@
+﻿(function (app) {
+    app.controller('searchController', searchController);
+
+    searchController.$inject = ['$scope', 'blockUI', '$modal', '$rootScope'];
+
+    function searchController($scope, blockUI, $modal, $rootScope) {
+        $scope.slider = {
+            minValue: 0,
+            maxValue: 50,
+            options: {
+                floor: 0,
+                ceil: 50,
+                step: 0.5,
+                precision: 1,
+                hideLimitLabels: true,
+                hidePointerLabels: true,
+            }
+        };
+    }
+})(angular.module('myApp'));
--- BoardingHouse.Web/app/app.js
+++ BoardingHouse.Web/app/app.js
@@ -1,6 +1,6 @@
 ﻿var app;
 (function () {
-    app = angular.module("myApp", ['blockUI', 'ui.bootstrap', 'ngAnimate', 'ngSanitize']);
+    app = angular.module("myApp", ['blockUI', 'ui.bootstrap', 'ngAnimate', 'ngSanitize', 'rzModule']);
     app.run(['$rootScope', function ($rootScope) {
         var baseUrl = $('baseurl').attr('value');
         $rootScope.baseUrl = baseUrl;
