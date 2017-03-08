9c56bae157e3d796ddf456fc9d3a2995c9dd56be
NamCkiku <tranhongnam11373@gmail.com>
2017-03-08 17:31:55 +0700

    room type

---
+++ BoardingHouse.Service/IService/IRoomTypeService.cs
@@ -0,0 +1,23 @@
+﻿using BoardingHouse.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardingHouse.Service.IService
+{
+    public interface IRoomTypeService
+    {
+        RoomType Add(RoomType info);
+
+        void Update(RoomType info);
+
+        RoomType Delete(int id);
+        IEnumerable<RoomType> GetAll();
+
+        RoomType GetById(int id);
+
+        void SaveChanges();
+    }
+}
