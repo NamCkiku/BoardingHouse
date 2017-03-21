8b20f775eef8fec74bdb0e2556b3762309ce3017
NamCkiku <tranhongnam11373@gmail.com>
2017-03-21 17:16:50 +0700

    add profile

---
+++ BoardingHouse.Web/app/Controller/ProfileController/ProfileController.js
@@ -0,0 +1,28 @@
+﻿(function (app) {
+    app.controller('ProfileController', ProfileController);
+
+    ProfileController.$inject = ['$scope', 'blockUI', '$modal', '$rootScope', 'BaseService', 'apiService', '$window'];
+
+    function ProfileController($scope, blockUI, $modal, $rootScope, BaseService, apiService, $window) {
+
+        angular.element(document).ready(function () {
+
+            $scope.load();
+        });
+        $scope.data = {
+            MainTab: 1
+        };
+        $scope.changeTab = function (tabIndex) {
+            return $scope.data.MainTab = tabIndex;
+        }
+
+        $scope.load = function () {
+            $scope.fireLoadProfileInformationEvent();
+        };
+
+        $scope.fireLoadProfileInformationEvent = function () {
+            $scope.changeTab(1);
+            $scope.$broadcast('fireLoadProfileInformationEvent',"");
+        };
+    }
+})(angular.module('myApp'));
