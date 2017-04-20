7d087f7414597452e1e634cb6652b9f896a231c8
Trần Hoàng Nam <namth@hn-binhanh.local>
2017-04-20 10:45:05 +0700

    add select2

--- BoardingHouse.Web/app/app.js
+++ BoardingHouse.Web/app/app.js
@@ -1,6 +1,6 @@
 ﻿var app;
 (function () {
-    app = angular.module("myApp", ['blockUI', 'ui.bootstrap', 'ngAnimate', 'ngSanitize', 'rzModule', 'ui.tinymce', 'thatisuday.dropzone', 'angularFileUpload', 'angularjs-dropdown-multiselect']);
+    app = angular.module("myApp", ['blockUI', 'ui.bootstrap', 'ngAnimate', 'ngSanitize', 'rzModule', 'ui.tinymce', 'thatisuday.dropzone', 'angularFileUpload', 'angularjs-dropdown-multiselect', 'ui.select2']);
     app.run(['$rootScope', function ($rootScope) {
         var baseUrl = $('baseurl').attr('value');
         $rootScope.baseUrl = baseUrl;
