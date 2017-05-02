c0bebe4881c4d2574790397b7adeaa1c05ed7df3
NamCkiku <tranhoangnam11373@gmail.com>
2017-05-03 06:59:09 +0700

    map

--- BoardingHouse.Web/app/Controller/MapController/mapController.js
+++ BoardingHouse.Web/app/Controller/MapController/mapController.js
@@ -1,109 +1,207 @@
 ﻿(function (app) {
     app.controller('mapController', mapController);
 
-    mapController.$inject = ['$scope', 'blockUI', '$modal', '$rootScope', 'BaseService', 'apiService', '$window'];
+    mapController.$inject = ['$scope', 'blockUI', '$modal', '$rootScope', 'BaseService', 'apiService', '$window', '$filter'];
 
-    function mapController($scope, blockUI, $modal, $rootScope, BaseService, apiService, $window) {
+    function mapController($scope, blockUI, $modal, $rootScope, BaseService, apiService, $window, $filter) {
         $scope.map;
         $scope.marker;
         $scope.uluru = { lat: 21.0029317912212212, lng: 105.820226663232323 };
         $scope.data = {
             lstRoom: [],
+            lstRoomType: [],
+            lstProvince: [],
+            lstDistrict: [],
+            lstWard: []
         }
-        $scope.GetAllRoom = GetAllRoom;
-        function GetAllRoom(page) {
-            page = page || 0;
-            var config = {
-                page: page,
-                pageSize: 12,
+        $scope.slider = {
+            minValue: 0,
+            maxValue: 50,
+            options: {
+                floor: 0,
+                ceil: 50,
+                step: 0.5,
+                precision: 1,
+                hideLimitLabels: true,
+                hidePointerLabels: true,
             }
-            apiService.post('Management/GetAllRoom', true, config, function (respone) {
+        };
+        $scope.select2Options = {
+            allowClear: true
+        };
+        $scope.keywords = {
+        };
+        function GetAllRoomType() {
+            apiService.post('Management/GetAllRoomType', true, null, function (respone) {
                 if (respone.data.success == true) {
-                    $scope.data.lstRoom = respone.data.lstData.Items;
-                    for (var i = 0; i < $scope.data.lstRoom.length; i++) {
-                        if ($scope.data.lstRoom[i].Lat !== null && $scope.data.lstRoom[i].Lng != null) {
-                            var latlng = new google.maps.LatLng($scope.data.lstRoom[i].Lat, $scope.data.lstRoom[i].Lng);
-                            console.log($scope.data.lstRoom[i].Lat, $scope.data.lstRoom[i].Lng);
-                            $scope.marker = new google.maps.Marker({
-                                position: latlng,
-                                map: $scope.map,
-                                animation: google.maps.Animation.DROP,
-                                icon: '/Content/img/IconMaker.png',
-                            });
-                            var contentString = '<div class="infoW"><div class="propImg"><div class="featured-label"><div class="featured-label-left"></div><div class="featured-label-content"><span class="fa fa-star"></span></div><div class="featured-label-right"></div><div class="clearfix"></div></div><img src="http://mariusn.com/themes/reales-wp/wp-content/uploads/2015/02/img-prop-400x240.jpg"><div class="propBg"><div class="propPrice">$799 000 </div><div class="propType">For Sale</div></div></div><div class="paWrapper"><div class="propTitle">Sophisticated Residence</div><div class="propAddress">600 40th Ave, San Francisco</div></div><ul class="propFeat"><li><span class="fa fa-moon-o"></span> 2</li><li><span class="icon-drop"></span> 3</li><li><span class="icon-frame"></span> 1270 sq ft</li></ul><div class="clearfix"></div><div class="infoButtons"><a class="btn btn-sm btn-round btn-gray btn-o closeInfo">Close</a><a href="http://mariusn.com/themes/reales-wp/properties/sophisticated-residence/" class="btn btn-sm btn-round btn-green viewInfo">View</a></div></div>'
-                            var infowindow = new google.maps.InfoWindow({
-                                content: contentString,
-                                disableAutoPan: false,
-                                maxWidth: 400,
-                                pixelOffset: new google.maps.Size(0, 0),
-                                zIndex: null,
-                                boxStyle: {
-                                    'background': '#fff',
-                                    'opacity': 1,
-                                    'padding': '5px',
-                                    'box-shadow': '0 1px 2px 0 rgba(0, 0, 0, 0.13)',
-                                    'width': '140px',
-                                    'text-align': 'center',
-                                    'border-radius': '3px'
-                                },
-                                closeBoxMargin: "28px 26px 0px 0px",
-                                closeBoxURL: "",
-                                infoBoxClearance: new google.maps.Size(1, 1),
-                                pane: "floatPane",
-                                enableEventPropagation: false
-                            });
-                            google.maps.event.addListener($scope.marker, 'click', (function (marker, i) {
-                                return function () {
-                                    infowindow.setContent(contentString);
-                                    infowindow.open($scope.map, marker);
-                                }
-                            })($scope.marker, i));
-                        }
-                        else {
-                            $scope.marker = new google.maps.Marker({
-                                position: $scope.uluru,
-                                map: $scope.map,
-                                animation: google.maps.Animation.DROP,
-                                icon: '/Content/img/IconMaker.png',
-                            });
-                        }
-                    }
+                    console.log(respone.data);
+                    $scope.data.lstRoomType = respone.data.lstData;
                 } else {
                 }
             }, function (respone) {
             });
         }
-        GetAllRoom();
-        //var locations = [
-        //{ lat: -31.563910, lng: 147.154312 },
-        //{ lat: -33.718234, lng: 150.363181 },
-        //{ lat: -33.727111, lng: 150.371124 },
-        //{ lat: -33.848588, lng: 151.209834 },
-        //{ lat: -33.851702, lng: 151.216968 },
-        //{ lat: -34.671264, lng: 150.863657 },
-        //{ lat: -35.304724, lng: 148.662905 },
-        //{ lat: -36.817685, lng: 175.699196 },
-        //{ lat: -36.828611, lng: 175.790222 },
-        //{ lat: -37.750000, lng: 145.116667 },
-        //{ lat: -37.759859, lng: 145.128708 },
-        //{ lat: -37.765015, lng: 145.133858 },
-        //{ lat: -37.770104, lng: 145.143299 },
-        //{ lat: -37.773700, lng: 145.145187 },
-        //{ lat: -37.774785, lng: 145.137978 },
-        //{ lat: -37.819616, lng: 144.968119 },
-        //{ lat: -38.330766, lng: 144.695692 },
-        //{ lat: -39.927193, lng: 175.053218 },
-        //{ lat: -41.330162, lng: 174.865694 },
-        //{ lat: -42.734358, lng: 147.439506 },
-        //{ lat: -42.734358, lng: 147.501315 },
-        //{ lat: -42.735258, lng: 147.438000 },
-        //{ lat: -43.999792, lng: 170.463352 }
-        //]
+        function GetAllProvince() {
+            apiService.post('Management/GetAllProvince', true, null, function (respone) {
+                if (respone.data.success == true) {
+                    console.log(respone.data);
+                    $scope.data.lstProvince = respone.data.lstData;
+                } else {
+                }
+            }, function (respone) {
+            });
+        }
+        $scope.GetAllDistrict = GetAllDistrict;
+        function GetAllDistrict(id) {
+            apiService.post('Management/GetAllDistrict', true, null, function (respone) {
+                if (respone.data.success == true) {
+                    console.log(respone.data);
+                    $scope.data.lstDistrict = $filter('filter')(respone.data.lstData, { provinceid: id }, true);
+                } else {
+                }
+            }, function (respone) {
+            });
+        }
+        $scope.GetAllWard = GetAllWard;
+        function GetAllWard(id) {
+            apiService.post('Management/GetAllWard', true, null, function (respone) {
+                if (respone.data.success == true) {
+                    console.log(respone.data);
+                    $scope.data.lstWard = $filter('filter')(respone.data.lstData, { districtid: id }, true);
+                } else {
+                }
+            }, function (respone) {
+            });
+        }
+        function formatPrice(nStr) {
+            nStr += '';
+            var x = nStr.split('.');
+            var x1 = x[0];
+            var x2 = x.length > 1 ? '.' + x[1] : '';
+            var rgx = /(\d+)(\d{3})/;
+            while (rgx.test(x1)) {
+                x1 = x1.replace(rgx, '$1' + ' ' + '$2');
+            }
+            return x1 + x2;
+        }
+        $scope.Search = Search;
+        function Search() {
+            GetAllRoom();
+        }
+        $scope.GetAllRoom = GetAllRoom;
+        function GetAllRoom(page) {
+            page = page || 0;
+            var config = {
+                RoomTypeID: $scope.keywords.RoomType,
+                PriceFrom: $scope.slider.minValue * 1000000,
+                PriceTo: $scope.slider.maxValue * 1000000,
+                WardID: $scope.keywords.Ward,
+                DistrictID: $scope.keywords.District,
+                ProvinceID: $scope.keywords.Province,
+                Keywords: $scope.keywords.Keywords,
+                page: page,
+                pageSize: 20,
+            }
+            apiService.post('Management/GetAllRoomFullSearch', true, config, function (respone) {
+                if (respone.data.success == true) {
+                    $scope.data.lstRoom = respone.data.lstData.Items;
+                    addMarkers($scope.data.lstRoom, $scope.map)
+                } else {
+                }
+            }, function (respone) {
+            });
+        }
+        GetAllProvince();
+        GetAllRoomType();
+        Search();
+        $scope.markers = [];
+        function addMarkers(props, map) {
+            var marker;
+            for (var i = 0; i < $scope.markers.length; i++) {
+                $scope.markers[i].setMap(null);
+            }
+            $.each(props, function (i, prop) {
+                var latlng = new google.maps.LatLng(prop.Lat, prop.Lng);
+                marker = new google.maps.Marker({
+                    position: latlng,
+                    map: map,
+                    icon: '/Content/img/IconMaker.png',
+                    draggable: false,
+                    animation: google.maps.Animation.DROP,
+                });
+                var infoboxContent = '<a href="/Home/RoomDetail/' + prop.RoomID + '"><div class="infoW">' +
+                                        '<div class="propImg">';
+                infoboxContent += '<div class="featured-label">' +
+                                                '<div class="featured-label-left"></div>' +
+                                                '<div class="featured-label-content"><span class="fa fa-star"></span></div>' +
+                                                '<div class="featured-label-right"></div>' +
+                                                '<div class="clearfix"></div>' +
+                                            '</div>';
+                infoboxContent += '<img src="http://localhost:6568/Content/images/' + prop.Image + '">' +
+                                            '<div class="propBg">' +
+                                                '<div class="propPrice">' + prop.RoomTypeName + '</div>';
+                if (prop.Price > 0) {
+                    infoboxContent += '<div class="propType">' + formatPrice(prop.Price) + 'VNĐ</div>';
+                }
+                infoboxContent += '</div>' +
+                                        '</div>' +
+                                        '<div class="paWrapper">' +
+                                            '<div class="propTitle">' + prop.RoomName + '</div>' +
+                                            '<div class="propAddress">';
+                if (prop.Address != '') {
+                    infoboxContent += prop.Address + ', ';
+                }
+                infoboxContent += '</div>' +
+                                        '</div>' +
+                                        '<ul class="propFeat">';
+                if (prop.BedroomNumber != '') {
+                    infoboxContent += '<li><span class="fa fa-moon-o"></span> ' + prop.BedroomNumber + '</li>';
+                }
+                if (prop.ToiletNumber != '') {
+                    infoboxContent += '<li><span class="fa fa-moon-o"></span> ' + prop.ToiletNumber + '</li>';
+                }
+                if (prop.Acreage != '') {
+                    infoboxContent += '<li><span class="fa fa-moon-o"></span> ' + formatPrice(prop.Acreage) + '</li>';
+                }
+                infoboxContent += '</ul></a>';
+                google.maps.event.addListener(marker, 'click', (function (marker, i) {
+                    return function () {
+                        infobox.setContent(infoboxContent);
+                        infobox.open(map, marker);
+                    }
+                })(marker, i));
+
+                $(document).on('touchend', '.closeInfo', function (e) {
+                    infobox.open(null, null);
+                });
+                $(document).on('click', '.closeInfo', function (e) {
+                    infobox.open(null, null);
+                });
+
+                $scope.markers.push(marker);
+            });
+        }
+        var infobox = new google.maps.InfoWindow({
+            disableAutoPan: false,
+            maxWidth: 202,
+            pixelOffset: new google.maps.Size(0, 0),
+            zIndex: null,
+            boxStyle: {
+                background: "url(http://mariusn.com/themes/reales-wp/wp-content/themes/reales/images/infobox-bg.png) no-repeat",
+                opacity: 1,
+                width: "202px",
+                height: "300px"
+            },
+            closeBoxMargin: "28px 26px 0px 0px",
+            closeBoxURL: "",
+            infoBoxClearance: new google.maps.Size(1, 1),
+            pane: "floatPane",
+            enableEventPropagation: false
+        });
         $scope.map = new google.maps.Map(document.getElementById('map'), {
             center: $scope.uluru,
             zoom: 14
         });
-
 
     }
 })(angular.module('myApp'));
