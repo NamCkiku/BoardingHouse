51764c5fa3bfad63468bfa4efc1fcc28ddf22523
namth <namth@PPM-NAMTH-PC>
2017-05-27 17:14:47 +0700

    dadasdasg

---
+++ BoardingHouse.Administrator/Controllers/RoomTypeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoardingHouse.Administrator.Controllers
+{
+    public class RoomTypeController : Controller
+    {
+        // GET: RoomType
+        public ActionResult RoomType()
+        {
+            return View();
+        }
+    }
+}
